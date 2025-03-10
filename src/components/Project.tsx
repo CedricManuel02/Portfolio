c55d914bb43629d4886725715b429113b305a943
Cedric Gio Manuel <cedricmanuel02@gmail.com>
2025-03-10 22:15:04 +0800

    update: Updated the image path

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,7 +1,7 @@
 import Movie from "../assets/Movie.png";
 import Project1 from "../assets/project1.png";
 import Project4 from "../assets/project4.png";
-import Project5 from "../assets/project5.png";
+import Project5 from "../assets/Project5.png";
 import Header from "./shared/Header";
 
 export default function Project() {
@@ -144,7 +144,7 @@ export default function Project() {
             <img
               className="h-64 rounded-xl transition-all hover:scale-[1.1] cursor-pointer  object-cover"
               src={Project4}
-              alt="Spotify Clone<"
+              alt="Spotify Clone"
             />
           </figure>
           <figcaption className="flex items-center justify-between py-4 gap-5">
